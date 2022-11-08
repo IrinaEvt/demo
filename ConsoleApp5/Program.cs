6b79cc9f066803c188efd56fd2e9128f063a7855
IrinaEvt <irra.evt@gmail.com>
2022-11-08 21:54:00 +0200

    Reading the .csv file

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+
+Console.WriteLine("Please, give the path to the file.");
+
+String path = @"C:\Users\irrae\Downloads\CustommerData.csv";
+
+try
+{
+    using(StreamReader reader = new StreamReader(path))
+    {
+        var line = reader.ReadLine();
+        while(line != null)
+        {
+            Console.WriteLine(line);
+            line = reader.ReadLine();
+        }
+    }
+}
+catch (Exception ex)
+{
+     Console.WriteLine(ex.Message);
+}
