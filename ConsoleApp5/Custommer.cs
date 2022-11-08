b40464cec7f0fc960375f9c57853dec3456265e8
IrinaEvt <irra.evt@gmail.com>
2022-11-09 00:06:13 +0200

    Final One

---
+++ ConsoleApp5/Custommer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Custommer
+    {
+        public Custommer(int iD, string firstName, string lastName, string email, string gender, string country, string city, string phone, string price, DateTime birthday)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Gender = gender;
+            Country = country;
+            City = city;
+            Phone = phone;
+            Price = price;
+            Birthday = birthday;
+        }
+
+        public  int ID { get; set; }
+        public  string FirstName { get; set; }
+        public  string LastName { get; set; }
+        public  string Email { get; set; }
+        public  string Gender { get; set; }
+        public  string Country { get; set; }
+        public  string City { get; set; }
+        public  string Phone { get; set; }
+        public  string Price { get; set; }
+        public  DateTime Birthday { get; set; }
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,23 +1,42 @@
-﻿using System.IO;
+﻿using Ganss.Excel;
+using System.IO;
+namespace ConsoleApp5 { 
+class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Please, give the path to the file.");
+            string path = Console.ReadLine();
+
+            //String path = @"C:\Users\irrae\Downloads\CustommerData.csv";
+
+            var custommers = new List<Custommer>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    var firstLine = reader.ReadLine();
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        string[] data = line.Split("|");
+                      
+                        Custommer custommer = new Custommer(int.Parse(data[0]), data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], DateTime.Parse(data[9]));
+                        custommers.Add(custommer);
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
 
-Console.WriteLine("Please, give the path to the file.");
-
-String path = @"C:\Users\irrae\Downloads\CustommerData.csv";
-
-try
-{
-    using(StreamReader reader = new StreamReader(path))
-    {
-        var line = reader.ReadLine();
-        while(line != null)
-        {
-            Console.WriteLine(line);
-            line = reader.ReadLine();
-        }
-    }
+            ExcelMapper mapper = new ExcelMapper();
+            var newFile = @"C:\Users\irrae\excel.xlsx";
+            mapper.Save(newFile, custommers, "SheetName", true);
 }
-catch (Exception ex)
-{
-     Console.WriteLine(ex.Message);
+}
 }
